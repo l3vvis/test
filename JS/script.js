71bec23d0ec0fa4a6f69f58b7ca43ae75ac40d24
l3vvis <example@yandex.ru>
2023-10-31 20:16:36 +0900

    19LessonPractice

--- JS/script.js
+++ JS/script.js
@@ -163,5 +163,33 @@
 // console.log(isCheck || !isClose);
 
 
-// 16/////////////////////////////////////////////////////////////
+// 19/////////////////////////////////////////////////////////////
+
+//19.1
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+//19.2
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+//19.3
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+// 20/////////////////////////////////////////////////////////////
 
