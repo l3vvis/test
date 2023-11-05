1601c0bbd17835adf52114ad4cbaff495d53ac9e
l3vvis <example@yandex.ru>
2023-11-05 18:42:14 +0900

    35LessonJS

--- JS/script.js
+++ JS/script.js
@@ -795,7 +795,110 @@
 
 /// 35/////////////////////////////////////////////////////////////
 
+    // let a = 5,
+    //     b = a;
 
+    // b = b + 4;
+
+    // console.log(b);
+    // console.log(a);
+
+    // const obj = {
+    //     a: 5,
+    //     b: 1
+    // };
+
+    // const copy = obj; // передаёт ссылку
+
+    // copy.a = 10;
+
+    // console.log(copy);
+    // console.log(obj);
+
+    ////////////
+
+    // function copy(mainObj) {
+    //     let objCopy = {};
+
+    //     let key;
+    //     for (key in mainObj) {
+    //         objCopy[key] = mainObj[key];
+    //     }
+    //     return objCopy;
+    // }
+
+    // ////////////////////
+
+    // const numbers = {
+    //     a: 2,
+    //     b: 5,
+    //     c: {
+    //         x: 7,
+    //         y: 9
+    //     }
+    // };
+
+    // const newNumbers = copy(numbers);
+
+    // newNumbers.a = 10;
+    // newNumbers.c.x = 15;
+
+    // // console.log(newNumbers);
+    // // console.log(numbers);
+
+    // //////////////////
+
+    // const add = {
+    //     d: 17,
+    //     e: 20
+    // };
+
+    // const clone = Object.assign({}, add);
+
+    // clone.d = 40;
+
+    // // console.log(add);
+    // // console.log(clone);
+
+    // const oldArray = ['a', 'b', 'c'];
+    // const newArray = oldArray.slice();
+
+    // newArray[1] = 'sasasasasasas';
+    // console.log(newArray);
+    // console.log(oldArray);
+
+    // //////////////////
+
+    // const video = ['youtube', 'vimeo', 'rutube'],
+    //     blogs = ['wordpress', 'JJ', 'blogger'],
+    //         internet = [...video, ...blogs, 'vk', 'telegramm'];
+
+    //         console.log(internet);
+
+    // ///////////////////
+
+    // function log (a, b, c) {
+    //     console.log(a);
+    //     console.log(b);
+    //     console.log(c);
+    // }
+
+    // const num = [2, 5, 7];
+
+    // log(...num);
+
+    ///////////////////
+
+    // const array = ['a', 'b'];
+
+    // const newAarray = [...array];
+
+    // const q = {
+    //     one: 1,
+    //     two: 2
+    // };
+
+    // const newObj = {...q};
 
 /// 36/////////////////////////////////////////////////////////////
 
