18f481535b4f200f4584e0b318aa5a24c0b365d7
l3vvis <example@yandex.ru>
2023-11-05 16:00:43 +0900

    33LessonJS

--- JS/script.js
+++ JS/script.js
@@ -696,11 +696,98 @@
 
 /// 32/////////////////////////////////////////////////////////////
 
+    // const obj = new Object();
+
+    // const options = {
+    //     name: 'test',
+    //     width: 1024,
+    //     height: 1024,
+    //     colors: {
+    //         border: 'black',
+    //         background: 'gray'
+    //     }
+    // };
+
+    // console.log(options.name);
     
+    // delete options.name;
+    
+    // console.log(options);
+
+    // let counter = 0;
+    // for (let key in options) {
+    //     if (typeof(options[key]) === 'object') {
+    //         for (let i in options[key]) {
+    //             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    //         }
+    //     } else {
+    //         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    //         counter++;
+    //     }
+    // }
+    // console.log(counter);
+
+    ///////////////
+
+    // const options = {
+    //     name: 'test',
+    //     width: 1024,
+    //     height: 1024,
+    //     colors: {
+    //         border: 'black',
+    //         background: 'gray'
+    //     },
+    //     makeTest: function() {
+    //         console.log('Test');
+    //     }
+    // };
+
+    // options.makeTest();
+
+    // const {border, background} = options.colors;
+    // console.log(border, background);
+
+    // console.log(Object.keys(options).length);
 
 /// 33/////////////////////////////////////////////////////////////
 
+    // const arr = [1, 22, 13, 6, 10];
+    // arr.sort(compareNum);  
+    // console.log(arr);
 
+    // function compareNum(a, b) { 
+    //     return a - b;
+    // }
+    // arr[99] = 0;
+
+    // console.log(arr.length);
+    // console.log(arr);
+
+    // arr.forEach(function(item, i, arr) {
+    //     console.log(`${i}: ${item} внутри массива ${arr}`);
+    // });
+
+    /////////////////////////////////////////////////////
+    
+    // arr.pop(); // - удаляет последний элемент в массиве
+    // arr.push(20); //- добавляет элемент в конец массива 
+
+    /////////////////////////////////////////////////////
+
+    // console.log(arr);
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     console.log(arr[i]);
+    // }
+
+    // for (let value of arr) { // - работает только с массиво-подобными сущностями
+    //     console.log(value);
+    // }
+
+    // const str = prompt('', '');
+    // const products = str.split(', ');
+    // products.sort();
+    // console.log(products.join('; '));
 
 /// 34/////////////////////////////////////////////////////////////
 
